5681c2fcd00b5ebbb0c3dd985611989ee5e85655
YuYang <yy2012cn@gmail.com>
2015-02-02 21:32:00 +0800

    initialize the repository

---
+++ settings.c
@@ -0,0 +1,12 @@
+/* -*- Mode: C; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*- */
+/*
+ * =======================================================================
+ *       Filename:  settings.c
+ *        Created:  2015-02-02 21:25:19
+ *       Compiler:  gcc
+ *
+ *         Author:  Yu Yang
+ *			Email:  yy2012cn@NOSPAM.gmail.com
+ * =======================================================================
+ */
+#include "settings.h"
